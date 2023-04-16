6b5647d02db190bd223ede3bcc8759a0435bc161
Kolby <mato.kollar2004@gmail.com>
2023-04-16 22:59:12 +0200

    added user buttons in left pane;

--- src/app/components/home-page/left-panel/left-panel.component.ts
+++ src/app/components/home-page/left-panel/left-panel.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-left-panel',
   templateUrl: './left-panel.component.html',
-  styleUrls: ['./left-panel.component.scss']
+  styleUrls: ['./left-panel.component.scss'],
 })
 export class LeftPanelComponent {
-
+  users = [
+    {
+      id: 2,
+      firstName: 'Emma',
+      lastName: 'Johnson',
+      email: 'emma.johnson@gmail.com',
+    },
+    {
+      id: 3,
+      firstName: 'David',
+      lastName: 'Lee',
+      email: 'david.lee@gmail.com',
+    },
+    {
+      id: 4,
+      firstName: 'Sarah',
+      lastName: 'Garcia',
+      email: 'sarah.garcia@gmail.com',
+    },
+    {
+      id: 5,
+      firstName: 'Michael',
+      lastName: 'Nguyen',
+      email: 'michael.nguyen@gmail.com',
+    },
+  ];
+  userClick(id: number): void {
+    console.log(id);
+  }
 }
